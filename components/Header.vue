95be0dc6bcd433f3cea0b2b8bdb4f404a92ddc5d
yozakura <matchnokaori@gmail.com>
2022-01-12 14:06:39 +0900

    tmdbからimage取得OK

---
+++ components/Header.vue
@@ -0,0 +1,100 @@
+<template>
+  <header>
+  <v-app-bar color="white" app>
+    <v-toolbar-title ><a href="/">Mana</a></v-toolbar-title>
+    <v-tabs>
+      <v-tab class="nav-links" v-for="nav_list in nav_lists" :key="nav_list.name" :to="nav_list.link" text>
+        {{ nav_list.name }}
+      </v-tab>
+    </v-tabs>
+    <v-spacer></v-spacer>
+    <v-btn color="#00A85F" class="white--text contact-btn" to="/contact">お問い合わせ</v-btn>
+    <v-app-bar-nav-icon @click="drawer=true"></v-app-bar-nav-icon>
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" right fixed temporary>
+    <v-container>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title grey--text text--darken-2">
+            <h3><a href="/">Mana</a></h3>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list nav>
+        <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name" :to="nav_list.link">
+          <template v-slot: activator text>
+            <v-list-item-content>{{ nav_list.name }}</v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-container>
+  </v-navigation-drawer>
+</header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      admins: [
+        {name: "GitHub", soto: "https://github.com/yozakurakirei"},
+        {name: "Qiita"},
+        {name: "ポートフォリオ"},
+        {name: "お問い合わせ", link: "/contact"}
+      ],
+      nav_lists: [
+        {name: "自社サービス", lists: ["フロントエンド", "バックエンド", "インフラ", "その他"]},
+        {name: "Sler"},
+        {name: "SES"},
+        {name: "その他企業"},
+        {name: "グラフで見てみる", link: "/graph"}
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-toolbar__title {
+  overflow: visible !important;
+  margin-right: 1rem !important;
+}
+
+.contact-btn {
+  font-weight: 700 !important;
+  margin-right: 1rem;
+}
+
+.drop__list {
+  border-bottom: 2px solid #ccc;
+  :hover {
+    opacity: .7;
+    cursor: pointer;
+  }
+  .drop__list__item {
+    font-size: .8rem !important;
+  }
+}
+
+// responsive
+.v-app-bar__nav-icon {
+  @include display_pc {
+    display: none !important;
+  }
+}
+
+.v-tabs {
+  display: none;
+
+  @include display_pc {
+    display: block !important;
+  }
+
+  :hover {
+    background: none;
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="home">
     <Hero />
+
+    <!-- movie -->
+    <div class="container movies">
+      <div id="movie-grid" class="movie-grid">
+        <div class="movie" v-for="(movie, index) in movies" :key="index">
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/w300/${movie.poster_path}`" alt="映画ポスター">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue"
+import axios from "axios"
 
 export default {
   name: "home",
   components: {
     Hero,
+  },
+  data() {
+    return {
+      movies: []
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=4f0c49dbd551a0791e957317b1237dc1&language=en-US&page=1`)
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    }
   }
 }
 </script>
